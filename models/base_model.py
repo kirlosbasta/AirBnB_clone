6cedd08db7a212e589f2051e65065bf3b9f8e0d3
kirlos basta <kirlosbasta@gmail.com>
2024-02-06 21:08:21 +0200

    Add base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Module that conatain class BaseModel"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''
+    class BaseModel that defines all common attributes/methods
+    for other classes
+    '''
+    def __init__(self):
+        '''
+        Initialize the id and created_at and updated_at
+        attributes for each inistance'''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''string representaion of of the class inistance'''
+        string = f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        return string
+
+    def save(self):
+        '''
+        updates the public instance attribute updated_at with the
+        current datetime
+        '''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''
+        returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        '''
+        dic = self.__dict__
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = datetime.isoformat(self.created_at)
+        dic['updated_at'] = datetime.isoformat(self.updated_at)
+        return dic
